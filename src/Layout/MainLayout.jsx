3f8ad886b76427f67deb8630b4bef9293bcccf0b
Devloper OPTS <devloper.opts@gmail.com>
2026-02-07 03:15:31 +0530

    .

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -162,7 +162,7 @@ function MainLayout({ children }) {
             {/* Overlay Header */}
             <div className="flex justify-between items-center p-6 pt-8 text-white">
                {/* Using logo image for Overlay as well to keep it consistent */}
-
+               <img className="h-12 object-contain  invert " src={logo} alt="OPTS Interior logo" />
                
                <div className="flex items-center gap-6">
                   <a href={`tel:${phoneNumber}`}>
